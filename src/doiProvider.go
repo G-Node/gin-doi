af35e352ca400bae660f815ee25b5ec2880f2408
cgars <christian@stuebeweg50.de>
2017-02-28 18:34:46 +0100

    Doi provider metadata xml generation

--- src/doiProvider.go
+++ src/doiProvider.go
@@ -3,17 +3,45 @@ package ginDoi
 import (
 	"net/http"
 	"bytes"
-	"fmt"
+	"text/template"
+	"log"
 )
 
+var LOGPREFIX = "DoiProvider"
 type DoiProvider struct {
 	//https://mds.datacite.org/static/apidoc
 	ApiURI string
+	Pwd string
+	DOIBase string
 }
 
-func (dp *DoiProvider) RegDoi(target string) (string, error){
-	bd := fmt.Sprintf("doi=12345\nurl=%s", target)
-	if r,err :=http.Post(dp.ApiURI,"text/plain;charset=UTF-8", bytes.NewBufferString(bd));err != nil{
+func (dp *DoiProvider) MakeDoi(doiInfo *DoiInfo) string {
+	doiInfo.DOI = dp.DOIBase + "/" + doiInfo.UUID[:10]
+	return doiInfo.DOI
+}
+
+func (dp *DoiProvider) GetXml(doiInfo DoiInfo) ([]byte, error) {
+	dp.MakeDoi(&doiInfo)
+	t, err := template.ParseFiles("tmpl/datacite.xml")
+	if err != nil{
+		log.Printf("[%s] Template broken:%s", LOGPREFIX, err)
+		return nil, err
+	}
+	buff := bytes.Buffer{}
+	err = t.Execute(&buff,doiInfo)
+	if err != nil{
+		log.Printf("[%s] template execution failed:%s", LOGPREFIX, err)
+		return nil, err
+	}
+	return buff.Bytes(), err
+}
+
+func (dp *DoiProvider) RegDoi(doiInfo DoiInfo) (string, error){
+	data, err := dp.GetXml(doiInfo)
+	if err != nil{
+		return "",err
+	}
+	if r,err :=http.Post(dp.ApiURI+"/metadata","text/plain;charset=UTF-8", bytes.NewReader(data));err != nil{
 		return "", err
 	}else {
 		return r.Status, nil
