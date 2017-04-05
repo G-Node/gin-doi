d2a727808626fdb0efc0316d552fe2111f44dc95
cgars <christian@stuebeweg50.de>
2017-04-05 14:24:35 +0200

    Test data source (rudimentary)

--- src/gindsource_test.go
+++ src/gindsource_test.go
@@ -3,16 +3,24 @@ package ginDoi
 import (
 	"os"
 	"testing"
+	"io/ioutil"
+	"log"
 )
 
 func TestGet(t *testing.T) {
-	ds := GinDataSource{}
-	os.Mkdir("test1", os.ModePerm)
-	out, err := ds.Get("../test_data/test1", "test1")
-	defer os.RemoveAll("test1")
+	tmpLoc, err := ioutil.TempDir("", "gin_testget")
+	if err != nil{
+		t.Log("[Err] Could nor create tempory directory for cloning")
+		t.Fail()
+		return
+	}
+	ds := GinDataSource{GinGitURL:""}
+	out, err := ds.Get("master:../contrib/test", tmpLoc)
+	defer os.RemoveAll(tmpLoc)
 	if err != nil {
 		t.Log(out)
 		t.Fail()
+		return
 	}
 	t.Log("[OK] Data source seems to clone")
 
@@ -22,4 +30,15 @@ func TestGet(t *testing.T) {
 		t.Fail()
 	}
 	t.Log("[OK] Data source seems to break")
+}
+
+func TestGetDoiInfo(t *testing.T) {
+	ds := GinDataSource{GinURL:"https://repo.gin.g-node.org/"}
+	cb, err := ds.GetDoiInfo("master:testi/test")
+	if err != nil{
+		log.Printf("[Err] Could nor get Doifile :%+v", err)
+	}
+	if (cb.Authors[0].FirstName=="Max") {
+		t.Log("[Ok]: Getting cloudberry seems fine")
+	}
 }
