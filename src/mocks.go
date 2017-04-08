dab724bdc8bee7a27283a1461aa69b924d95c0c2
cgars <christian@stuebeweg50.de>
2017-04-08 17:18:43 +0200

    Mocks

---
+++ src/mocks.go
@@ -0,0 +1,37 @@
+package ginDoi
+
+import (
+	"os"
+	"fmt"
+)
+
+type MockDataSource struct {
+	calls []string
+
+}
+func (ds *MockDataSource) ValidDoiFile(URI string) (bool, *CBerry){
+	return true, &CBerry{}
+}
+func (ds *MockDataSource) Get(URI string, To string) (string, error) {
+	os.Mkdir(To, os.ModePerm)
+	ds.calls = append(ds.calls, fmt.Sprintf("%s, %s", URI, To))
+	return "", nil
+}
+func (ds *MockDataSource) MakeUUID(URI string) (string, error) {
+	return "123", nil
+}
+
+type MockDoiProvider struct {
+
+}
+
+func (dp MockDoiProvider) MakeDoi(doiInfo *CBerry) string{
+	return "133"
+}
+func (dp MockDoiProvider) GetXml(doiInfo *CBerry) ([]byte, error) {
+	return []byte("xml"), nil
+}
+func (dp MockDoiProvider) RegDoi(doiInfo CBerry) (string, error){
+	return "", nil
+}
+
