10795229435ccb17fd5fdd2bd56b9b663f859f54
cgars <christian@stuebeweg50.de>
2017-05-11 15:58:16 +0200

    Oauth provider mock

--- src/mocks.go
+++ src/mocks.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"os"
 	"crypto/rsa"
+	"github.com/G-Node/gin-core/gin"
 )
 
 type MockDataSource struct {
-	calls []string
+	calls        []string
+	validDoiFile bool
+	Berry        CBerry
 }
 
 func (ds *MockDataSource) ValidDoiFile(URI string, user OauthIdentity) (bool, *CBerry) {
-	return true, &CBerry{}
+	return ds.validDoiFile, &ds.Berry
 }
 func (ds *MockDataSource) Get(URI string, To string, key *rsa.PrivateKey) (string, error) {
 	os.Mkdir(To, os.ModePerm)
@@ -34,4 +37,25 @@ func (dp MockDoiProvider) GetXml(doiInfo *CBerry) (string, error) {
 }
 func (dp MockDoiProvider) RegDoi(doiInfo CBerry) (string, error) {
 	return "", nil
+}
+
+type MockOauthProvider struct {
+	ValidToken bool
+	User       OauthIdentity
+}
+
+func (op MockOauthProvider) ValidateToken(userName string, token string) (bool, error) {
+	return op.ValidToken, nil
+}
+
+func (op MockOauthProvider) getUser(userName string, token string) (OauthIdentity, error) {
+	return op.User, nil
+}
+
+func (op MockOauthProvider) AuthorizePull(user OauthIdentity) (*rsa.PrivateKey, error) {
+	return &rsa.PrivateKey{}, nil
+}
+
+func (op MockOauthProvider) DeAuthorizePull(user OauthIdentity, key gin.SSHKey) (error) {
+	return nil
 }
