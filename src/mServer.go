cb930d62e4201fbf29c3a0fc1ce3d1e15042869e
cgars <christian@stuebeweg50.de>
2017-06-28 16:42:46 +0200

    Non authenticated smtp for mails

--- src/mServer.go
+++ src/mServer.go
@@ -3,6 +3,8 @@ package ginDoi
 import (
 	log "github.com/Sirupsen/logrus"
 	"net/smtp"
+	"strings"
+	"bytes"
 )
 
 var (
@@ -17,12 +19,40 @@ type MailServer struct {
 }
 
 func (ms *MailServer) ToMaster(content string) error {
-	auth := smtp.PlainAuth("", "", "", ms.Adress)
 	if ms.DoSend {
-		return smtp.SendMail(ms.Adress, auth, ms.From, []string{ms.Master}, []byte(content))
+		c, err := smtp.Dial(ms.Adress)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"source": MAILLOG,
+				"error":  err,
+			}).Errorf("Could not reach server")
+			return err
+		}
+		defer c.Close()
+		// Set the sender and recipient.
+		c.Mail(ms.From)
+		c.Rcpt(ms.Master)
+		// Send the email body.
+		wc, err := c.Data()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"source": MAILLOG,
+				"error":  err,
+			}).Errorf("Could not write Mail")
+			return err
+		}
+		defer wc.Close()
+		buf := bytes.NewBufferString(content)
+		if _, err = buf.WriteTo(wc); err != nil {
+			log.WithFields(log.Fields{
+				"source": MAILLOG,
+				"error":  err,
+			}).Errorf("Could not write Mail")
+		}
+	} else{
+		log.WithFields(log.Fields{
+			"source": MAILLOG,
+		}).Infof("Fake Mail to: %s, content: %s, Auth:%+v", ms.Master, content)
 	}
-	log.WithFields(log.Fields{
-		"source": MAILLOG,
-	}).Infof("Fake Mail to: %s, content: %s, Auth:%+v", ms.Master, content, auth)
 	return nil
 }
