379a51ac0d8cdb9645ddaa0ee49bb25ee1ae57ec
Achilleas Koutsou <achilleas.k@gmail.com>
2018-10-17 15:13:36 +0200

    [templates] Propagating variable name changes to templates

--- tmpl/updIndex.sh
+++ tmpl/updIndex.sh
@@ -1,7 +1,7 @@
 sed "35i <tr>\
 <td>\
-<a href=\"{{.DoiInfo.UUID}}\">{{.DoiInfo.Title}}</a>\
+<a href=\"{{.DOIInfo.UUID}}\">{{.DOIInfo.Title}}</a>\
 </td>\
 <td>2017-02-01</td>\
-<td><a href=\"https://doi.org/{{.DoiInfo.DOI}}\" class =\"label label-default\">{{.DoiInfo.DOI}}</a></td>\
+<td><a href=\"https://doi.org/{{.DOIInfo.DOI}}\" class =\"label label-default\">{{.DOIInfo.DOI}}</a></td>\
 </tr>" ../index.html
