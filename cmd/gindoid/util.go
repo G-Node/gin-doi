f52c4528971d6d67ecb58e95ca90ddec8635be32
Achilleas Koutsou <achilleas.k@gmail.com>
2020-01-08 16:32:22 +0100

    [util] Functions for XML template rendering
    - EscXML: Used to be a method of DOIRegInfo, but it didn't use any of
    the struct's fields, so there was no reason for it to be a method.
    - ReferenceDescription: Produces a single string representation for a
    reference. This will be added to the descriptions list in the XML with
    type "other", so the full citation can be associated with the
    publication.

--- cmd/gindoid/util.go
+++ cmd/gindoid/util.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/md5"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/G-Node/libgin/libgin"
 )
 
 func readBody(r *http.Request) (*string, error) {
@@ -65,4 +70,31 @@ func makeUUID(URI string) string {
 	}
 	currMd5 := md5.Sum([]byte(URI))
 	return hex.EncodeToString(currMd5[:])
+}
+
+// EscXML runs a string through xml.EscapeText.
+// This is a utility function for the doi.xml template.
+func EscXML(txt string) string {
+	buf := new(bytes.Buffer)
+	if err := xml.EscapeText(buf, []byte(txt)); err != nil {
+		log.Printf("Could not escape: %q :: %s", txt, err.Error())
+		return ""
+	}
+	return buf.String()
+}
+
+// ReferenceDescription creates a string representation of a reference for use in the XML description tag.
+// This is a utility function for the doi.xml template.
+func ReferenceDescription(ref libgin.Reference) string {
+	var namecitation string
+	if ref.Name != "" && ref.Citation != "" {
+		namecitation = ref.Name + " " + ref.Citation
+	} else {
+		namecitation = ref.Name + ref.Citation
+	}
+
+	if !strings.HasSuffix(namecitation, ".") {
+		namecitation += "."
+	}
+	return fmt.Sprintf("%s: %s (%s)", ref.Reftype, namecitation, ref.ID)
 }
