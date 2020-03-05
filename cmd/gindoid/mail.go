89d050ec557cee981a0e1c8402ab4039bf5ab426
Achilleas Koutsou <achilleas.k@gmail.com>
2020-03-05 14:03:12 +0100

    Simplify sendMail function

--- cmd/gindoid/mail.go
+++ cmd/gindoid/mail.go
@@ -77,55 +77,55 @@ func notifyAdmin(job *RegistrationJob, errors []string) error {
 // configuration specifies the server to use, the from address, and a file that
 // lists the addresses of the recipients.
 func sendMail(subject, body string, conf *Configuration) error {
-	if conf.Email.Server != "" {
-		log.Print("Preparing mail")
-		c, err := smtp.Dial(conf.Email.Server)
-		if err != nil {
-			log.Print("Could not reach server")
-			return err
-		}
-		defer c.Close()
-		// Set the sender and recipient.
-		c.Mail(conf.Email.From)
-		message := fmt.Sprintf("From: %s\nSubject: %s", conf.Email.From, subject)
-
-		// Recipient list is read every time a sendMail() is called.
-		// This way, the recipient list can be changed without restarting the service.
-		emailfile, err := os.Open(conf.Email.RecipientsFile)
-		if err == nil {
-			defer emailfile.Close()
-			filereader := bufio.NewReader(emailfile)
-			for address, lerr := filereader.ReadString('\n'); lerr == nil; address, lerr = filereader.ReadString('\n') {
-				address = strings.TrimSpace(address)
-				log.Printf("To: %s", address)
-				c.Rcpt(address)
-				message = fmt.Sprintf("%s\nTo: %s", message, address)
-			}
-		} else {
-			log.Printf("Email file %s could not be read: %s", conf.Email.RecipientsFile, err.Error())
-			log.Printf("Notifying %s", DEFAULTTO)
-			log.Printf("To: %s", DEFAULTTO)
-			c.Rcpt(DEFAULTTO)
-			message = fmt.Sprintf("%s\nTo: %s", message, DEFAULTTO)
-		}
-
-		message = fmt.Sprintf("%s\n\n%s", message, body)
-		// Send the email body.
-		log.Print("Sending mail")
-
-		wc, err := c.Data()
-		if err != nil {
-			log.Print("Could not write mail")
-			return err
-		}
-		defer wc.Close()
-		buf := bytes.NewBufferString(message)
-		if _, err = buf.WriteTo(wc); err != nil {
-			log.Print("Could not write mail")
-		}
-		log.Print("sendMail Done")
-	} else {
+	if conf.Email.Server == "" {
 		log.Printf("Fake mail body: %s", body)
+		return nil
 	}
+	log.Print("Preparing mail")
+	c, err := smtp.Dial(conf.Email.Server)
+	if err != nil {
+		log.Print("Could not reach server")
+		return err
+	}
+	defer c.Close()
+	// Set the sender and recipient.
+	c.Mail(conf.Email.From)
+	message := fmt.Sprintf("From: %s\nSubject: %s", conf.Email.From, subject)
+
+	// Recipient list is read every time a sendMail() is called.
+	// This way, the recipient list can be changed without restarting the service.
+	emailfile, err := os.Open(conf.Email.RecipientsFile)
+	if err == nil {
+		defer emailfile.Close()
+		filereader := bufio.NewReader(emailfile)
+		for address, lerr := filereader.ReadString('\n'); lerr == nil; address, lerr = filereader.ReadString('\n') {
+			address = strings.TrimSpace(address)
+			log.Printf("To: %s", address)
+			c.Rcpt(address)
+			message = fmt.Sprintf("%s\nTo: %s", message, address)
+		}
+	} else {
+		log.Printf("Email file %s could not be read: %s", conf.Email.RecipientsFile, err.Error())
+		log.Printf("Notifying %s", DEFAULTTO)
+		log.Printf("To: %s", DEFAULTTO)
+		c.Rcpt(DEFAULTTO)
+		message = fmt.Sprintf("%s\nTo: %s", message, DEFAULTTO)
+	}
+
+	message = fmt.Sprintf("%s\n\n%s", message, body)
+	// Send the email body.
+	log.Print("Sending mail")
+
+	wc, err := c.Data()
+	if err != nil {
+		log.Print("Could not write mail")
+		return err
+	}
+	defer wc.Close()
+	buf := bytes.NewBufferString(message)
+	if _, err = buf.WriteTo(wc); err != nil {
+		log.Print("Could not write mail")
+	}
+	log.Print("sendMail Done")
 	return nil
 }
