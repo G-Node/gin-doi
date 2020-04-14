4849789bd8ef3bb2d05fc4232e47268329b9a97e
Achilleas Koutsou <achilleas.k@gmail.com>
2020-04-14 23:14:19 +0200

    Request page: Move the page title into main container
    Fixes the padding between the Welcome message and the info box.

--- templates/reqpage.go
+++ templates/reqpage.go
@@ -51,13 +51,10 @@ const RequestPage = `<!DOCTYPE html>
 				</div>
 			</div>
 			<div class="home middle very relaxed page grid" id="main">
-				<div class="ui vertically padded head">
+				<div class="ui container wide centered column doi">
 					<div class="column center">
 						<h1>Welcome to the GIN DOI service <i class="mega-octicon octicon octicon-squirrel"></i></h1>
 					</div>
-				</div>
-
-				<div class="ui container wide centered column doi">
 					<div class="ui info message" id="infotable">
 						<div id="infobox">
 							The following is a <strong>preview</strong> of the information page for your published repository.
