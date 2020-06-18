85c373e3558c09964e1c2f8891b821309fab9135
Achilleas Koutsou <achilleas.k@gmail.com>
2020-06-18 19:33:02 +0200

    Test: Separate each template test

--- cmd/gindoid/templaterender_test.go
+++ cmd/gindoid/templaterender_test.go
@@ -8,40 +8,41 @@ import (
 	"github.com/G-Node/libgin/libgin"
 )
 
-func TestTemplateRendering(t *testing.T) {
-	// RequestFailurePage
+func TestRequestFailureTemplate(t *testing.T) {
 	regRequest := new(RegistrationRequest)
 	regRequest.Message = template.HTML(msgInvalidRequest)
-	regRequest.Metadata = &libgin.RepositoryMetadata{}
+	regRequest.Metadata = new(libgin.RepositoryMetadata)
 	tmpl, err := prepareTemplates("RequestFailurePage")
 	if err != nil {
-		t.Fatalf("Failed to parse requestpage template: %s", err.Error())
+		t.Fatalf("Failed to parse RequestFailureP template: %s", err.Error())
 	}
 
 	w := new(bytes.Buffer)
 	if err := tmpl.Execute(w, regRequest); err != nil {
-		t.Log(string(w.Bytes())) // Print the rendered output
+		t.Log(w.String()) // Print the rendered output
 		t.Fatalf("Failed to execute RequestFailurePage: %s", err.Error())
 	}
+}
 
-	// Request preview page
-	tmpl, err = prepareTemplates("DOIInfo", "RequestPage")
+func TestRequestPageTemplate(t *testing.T) {
+	tmpl, err := prepareTemplates("DOIInfo", "RequestPage")
 	if err != nil {
-		t.Fatalf("Failed to parse requestpage template: %s", err.Error())
+		t.Fatalf("Failed to parse DOIInfo, RequestPage templates: %s", err.Error())
 	}
-	w = new(bytes.Buffer)
 	// read datacite.yml template for test
 	infoyml, err := readFileAtURL("https://gin.g-node.org/G-Node/Info/raw/master/datacite.yml")
 	if err != nil {
 		t.Fatalf("Failed to retrieve datacite.yml from GIN")
 	}
 	doiInfo, err := readRepoYAML(infoyml)
+	regRequest := new(RegistrationRequest)
 	regRequest.DOIRequestData = &libgin.DOIRequestData{
 		Username:   "testuser",
 		Realname:   "Test User",
 		Repository: "user/test",
 		Email:      "doitest@example.org",
 	}
+	regRequest.Metadata = new(libgin.RepositoryMetadata)
 	regRequest.Metadata.YAMLData = doiInfo
 	regRequest.Metadata.DataCite = libgin.NewDataCiteFromYAML(doiInfo)
 	regRequest.Metadata.SourceRepository = regRequest.DOIRequestData.Repository
@@ -49,7 +50,7 @@ func TestTemplateRendering(t *testing.T) {
 
 	err = tmpl.Execute(w, regRequest)
 	if err := tmpl.Execute(w, regRequest); err != nil {
-		t.Log(string(w.Bytes())) // Print the rendered output
+		t.Log(w.String()) // Print the rendered output
 		t.Fatalf("Failed to execute RequestPage: %s", err.Error())
 	}
 }
