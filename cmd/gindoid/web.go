6771a21e59ae46fd403efe1ae987e17a15580466
Achilleas Koutsou <achilleas.k@gmail.com>
2020-09-10 11:14:04 +0200

    Move message constants to separate file

--- cmd/gindoid/web.go
+++ cmd/gindoid/web.go
@@ -15,50 +15,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	msgInvalidRequest    = `Invalid request data received.  Please note that requests should only be submitted through repository pages on <a href="https://gin.g-node.org">GIN</a>.  If you followed the instructions in the <a href="https://gin.g-node.org/G-Node/Info/wiki/DOIfile">DOI registration guide</a> and arrived at this error page, please <a href="mailto:gin@g-node.org">contact us</a> for assistance.`
-	msgInvalidDOI        = `The DOI file is missing or not valid. See the messages below for specific issues with the provided data.<br>Also, please see <a href="https://gin.g-node.org/G-Node/Info/wiki/DOIfile">the DOI guide</a> for detailed instructions.`
-	msgInvalidURI        = "Please provide a valid repository URI"
-	msgAlreadyRegistered = `<div class="content">
-								<div class="header"> A DOI is already registered for your dataset.</div>
-								Your DOI is: <br>
-								<div class ="ui label label-default"><a href="https://doi.org/%s">%s</a></div></br>
-								If this is incorrect or you would like to register a new version of your dataset, please <a href=mailto:gin@g-node.org>contact us</a>.
-							</div>`
-	msgServerIsArchiving = `<div class="content">
-			<div class="header">The DOI server has started archiving your repository.</div>
-		We have reserved the following DOI for your dataset:<br>
-		<div class ="ui label label-default">%s</div><br>
-		In rare cases the final DOI might be different.<br>
-		Please note that the final step in the registration process requires us to manually review your request.
-		It may therefore take a few hours until the DOI is finally registered and your data becomes available.
-		We will notify you via email once the process is finished.<br>
-		<div class="ui tabs divider"> </div>
-		<b>This page can safely be closed. You do not need to keep it open.</b>
-		</div>
-		`
-	msgNotLoggedIn      = `You are not logged in with the gin service. Login <a href="http://gin.g-node.org/">here</a>`
-	msgNoToken          = "No authentication token provided"
-	msgNoUser           = "No username provided"
-	msgNoTitle          = "No title provided."
-	msgNoAuthors        = "No authors provided."
-	msgInvalidAuthors   = "Not all authors valid. Please provide at least a last name and a first name."
-	msgNoDescription    = "No description provided."
-	msgNoLicense        = "No valid license provided. Please specify a license URL and name and make sure it matches the license file in the repository."
-	msgNoLicenseFile    = `The LICENSE file is missing. The full text of the license is required to be in the repository when publishing. See the <a href="https://gin.g-node.org/G-Node/Info/wiki/Licensing">Licensing</a> help page for details and links to recommended data licenses.`
-	msgLicenseMismatch  = `The LICENSE file does not match the license specified in the metadata. See the <a href="https://gin.g-node.org/G-Node/Info/wiki/Licensing">Licensing</a> help page for links to full text for available licenses.`
-	msgInvalidReference = "One of the Reference entries is not valid. Please provide the full citation and type of the reference."
-	msgBadEncoding      = `There was an issue with the content of the DOI file (datacite.yml). This might mean that the encoding is wrong. Please see <a href="https://gin.g-node.org/G-Node/Info/wiki/DOIfile">the DOI guide</a> for detailed instructions or contact gin@g-node.org for assistance.`
-
-	msgSubmitError     = "An internal error occurred while we were processing your request.  The G-Node team has been notified of the problem and will attempt to repair it and process your request.  We may contact you for further information regarding your request.  Feel free to <a href=mailto:gin@g-node.org>contact us</a> if you would like to provide more information or ask about the status of your request."
-	msgSubmitFailed    = "An internal error occurred while we were processing your request.  Your request was not submitted and the service failed to notify the G-Node team.  Please <a href=mailto:gin@g-node.org>contact us</a> to report this error."
-	msgNoTemplateError = "An internal error occurred while we were processing your request.  The G-Node team has been notified of the problem and will attempt to repair it and process your request.  We may contact you for further information regarding your request.  Feel free to contact us at gin@g-node.org if you would like to provide more information or ask about the status of your request."
-	// Log Prefixes
-	lpAuth    = "GinOAP"
-	lpStorage = "Storage"
-	lpMakeXML = "MakeXML"
-)
-
 type reqResultData struct {
 	Success    bool
 	Level      string // success, warning, error
