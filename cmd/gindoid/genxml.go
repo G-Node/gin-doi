64c91fc60569585b67075895cf1ca7e62dc7f5d8
M. Sonntag <michael.p.sonntag@gmail.com>
2021-05-10 21:55:51 +0200

    [genxml] Add getGINDataciteURL func

--- cmd/gindoid/genxml.go
+++ cmd/gindoid/genxml.go
@@ -10,21 +10,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// cleanupGINURL returns owner and repository of a GIN
-// repository datacite URL string. Example:
-// https://gin.g-node.org/G-Node/doi_deployment_test/raw/master/datacite.yml
-func cleanupGINURL(input string) (string, error) {
-	cleanstr := strings.Replace(input, "https://gin.g-node.org/", "", -1)
-	cleanstr = strings.Replace(cleanstr, "/raw/master/datacite.yml", "", -1)
-	out := strings.Split(cleanstr, "/")
-
-	if len(out) != 2 {
-		return "", fmt.Errorf("could not parse URL: %s", input)
+// getGINDataciteURL returns a full URL to the datacite file at
+// the root of a GIN repository where owner and repository name
+// were provided as a single input string.
+func getGINDataciteURL(input string) (string, error) {
+	inputslice := strings.Split(input, "/")
+	if len(inputslice) != 2 {
+		return "", fmt.Errorf("could not parse gin repo string %s", input)
 	}
-	return out[1], nil
+
+	ginprefix := "https://gin.g-node.org/"
+	ginpostfix := "/raw/master/datacite.yml"
+	out := fmt.Sprintf("%s%s%s", ginprefix, input, ginpostfix)
+
+	return out, nil
 }
 
-// mkxml reads one or more datacite YAML files from GIN, a provided URL or from 
+// mkxml reads one or more datacite YAML files from GIN, a provided URL or from
 // a direct file and generates a Datacite XML file for each.
 // Reading files from GIN requires only the repository owner and the repository name
 // of the GIN repository prefixed with GIN in the format "GIN:[owner]/[repository]"
@@ -36,11 +38,23 @@ func mkxml(cmd *cobra.Command, args []string) {
 		var contents []byte
 		var err error
 		var repoName string
-		if isURL(filearg) {
-			repoName, err = cleanupGINURL(filearg)
+		if strings.HasPrefix(filearg, "GIN:") {
+			repostring := strings.Replace(filearg, "GIN:", "", 1)
+			ginurl, err := getGINDataciteURL(repostring)
 			if err != nil {
-				fmt.Printf("failed to cleanup GIN datacite URL: %s", err.Error())
+				fmt.Printf("Failed to parse GIN specific repo string: %s\n", filearg)
+				continue
 			}
+			repodata := strings.Split(repostring, "/")
+			if len(repodata) == 2 {
+				repoName = repodata[1]
+			}
+			contents, err = readFileAtURL(ginurl)
+			if err != nil {
+				fmt.Printf("Failed to read file at %q: %s\n", ginurl, err.Error())
+				continue
+			}
+		} else if isURL(filearg) {
 			contents, err = readFileAtURL(filearg)
 		} else {
 			contents, err = readFileAtPath(filearg)
@@ -52,7 +66,7 @@ func mkxml(cmd *cobra.Command, args []string) {
 
 		dataciteContent, err := readRepoYAML(contents)
 		if err != nil {
-			fmt.Print("DOI file invalid")
+			fmt.Print("DOI file invalid\n")
 			continue
 		}
 
