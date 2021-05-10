a78d98144aa9133fa4cc6106d128e8f894f29a20
M. Sonntag <michael.p.sonntag@gmail.com>
2021-05-10 21:31:29 +0200

    [genxml] Add function body

--- cmd/gindoid/genxml.go
+++ cmd/gindoid/genxml.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/G-Node/libgin/libgin"
 	"github.com/spf13/cobra"
 )
 
@@ -12,4 +15,63 @@ import (
 // of the GIN repository prefixed with GIN in the format "GIN:[owner]/[repository]"
 func mkxml(cmd *cobra.Command, args []string) {
 	fmt.Printf("Generating %d xml files\n", len(args))
+	var success int
+	for idx, filearg := range args {
+		fmt.Printf("%3d: %s\n", idx, filearg)
+		var contents []byte
+		var err error
+		var repoName string
+		if isURL(filearg) {
+			contents, err = readFileAtURL(filearg)
+		} else {
+			contents, err = readFileAtPath(filearg)
+		}
+		if err != nil {
+			fmt.Printf("Failed to read file at %q: %s\n", filearg, err.Error())
+			continue
+		}
+
+		dataciteContent, err := readRepoYAML(contents)
+		if err != nil {
+			fmt.Print("DOI file invalid")
+			continue
+		}
+
+		datacite := libgin.NewDataCiteFromYAML(dataciteContent)
+
+		// Create storage directory
+		if repoName == "" {
+			repoName = fmt.Sprintf("index-%03d", idx)
+		}
+		fname := filepath.Join(repoName, "doi.xml")
+		if err = os.MkdirAll(repoName, 0777); err != nil {
+			fmt.Printf("WARNING: Could not create directory %s: %q", repoName, err.Error())
+			fname = fmt.Sprintf("%s-doi.xml", repoName)
+		}
+
+		fp, err := os.Create(fname)
+		if err != nil {
+			// XML Creation failed; return with error
+			fmt.Printf("Failed to create the XML metadata file: %s", err)
+			continue
+		}
+		defer fp.Close()
+
+		data, err := datacite.Marshal()
+		if err != nil {
+			fmt.Printf("Failed to render the XML metadata file: %s", err)
+			continue
+		}
+		_, err = fp.Write([]byte(data))
+		if err != nil {
+			fmt.Printf("Failed to write the metadata XML file: %s", err)
+			continue
+		}
+
+		fmt.Printf("\t-> %s\n", fname)
+		// all good
+		success++
+	}
+
+	fmt.Printf("%d/%d jobs completed successfully\n", success, len(args))
 }
