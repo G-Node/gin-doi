b9af6bf33f5cd7bd8bc645c594b67163cc6a2af9
M. Sonntag <michael.p.sonntag@gmail.com>
2022-01-25 15:45:24 +0100

    [test/checklist]

--- cmd/gindoid/checklist_test.go
+++ cmd/gindoid/checklist_test.go
@@ -1,16 +1,64 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/G-Node/libgin/libgin"
 )
 
 func TestChecklistFromMetadata(t *testing.T) {
-	// assert no issue on blank input
+	// assert no panic on nil input
+	_, err := checklistFromMetadata(nil, "")
+	if err == nil || !strings.Contains(err.Error(), "encountered libgin.RepositoryMetadata nil pointer") {
+		t.Fatalf("Expected function to fail gracefully with nil pointer error: %v", err)
+	}
+
+	// assert no panic on blank RepositoryMetadata struct, nil sub-level structs
 	md := new(libgin.RepositoryMetadata)
-	_, err := checklistFromMetadata(md, "")
-	if err == nil {
-		t.Fatalf("Expected function to fail gracefully")
+	_, err = checklistFromMetadata(md, "")
+	if err == nil || !strings.Contains(err.Error(), "encountered libgin.RepositoryMetadata nil pointer") {
+		t.Fatalf("Expected function to fail gracefully with nil pointer error: %v", err)
+	}
+
+	// assert no panic on nil/blank combinations
+	md.DataCite = new(libgin.DataCite)
+	_, err = checklistFromMetadata(md, "")
+	if err == nil || !strings.Contains(err.Error(), "encountered libgin.RepositoryMetadata nil pointer") {
+		t.Fatalf("Expected function to fail gracefully with nil pointer error: %v", err)
+	}
+
+	md.DataCite = nil
+	md.YAMLData = new(libgin.RepositoryYAML)
+	_, err = checklistFromMetadata(md, "")
+	if err == nil || !strings.Contains(err.Error(), "encountered libgin.RepositoryMetadata nil pointer") {
+		t.Fatalf("Expected function to fail gracefully with nil pointer error: %v", err)
+	}
+
+	md.DataCite = new(libgin.DataCite)
+	_, err = checklistFromMetadata(md, "")
+	if err == nil || !strings.Contains(err.Error(), "encountered libgin.RequestingUser nil pointer") {
+		t.Fatalf("Expected function to fail gracefully with nil pointer error: %v", err)
+	}
+
+	// assert no panic on empty SourceRepository
+	md.RequestingUser = new(libgin.GINUser)
+	_, err = checklistFromMetadata(md, "")
+	if err == nil || !strings.Contains(err.Error(), "cannot parse SourceRepository") {
+		t.Fatalf("Expected function to fail gracefully on missing SourceRepository: %v", err)
+	}
+
+	// assert no panic on blank DataCite Dates
+	md.SourceRepository = "a/b"
+	_, err = checklistFromMetadata(md, "")
+	if err == nil || !strings.Contains(err.Error(), "missing pubication date") {
+		t.Fatalf("Expected function to fail gracefully on missing DataCite.Dates: %v", err)
+	}
+
+	// assert no panic on blank libgin.User
+	md.DataCite.Dates = append(md.DataCite.Dates, libgin.Date{})
+	_, err = checklistFromMetadata(md, "")
+	if err != nil {
+		t.Fatalf("%s", err.Error())
 	}
 }
