e837b03875233580f6d9d6a5c11bede1a779f462
M. Sonntag <michael.p.sonntag@gmail.com>
2022-01-31 15:48:11 +0100

    [test/sitemap] Add TestMKSitemap

--- cmd/gindoid/gensitemap_test.go
+++ cmd/gindoid/gensitemap_test.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path/filepath"
 	"sort"
 	"testing"
 )
@@ -41,5 +47,76 @@ func TestURLlist(t *testing.T) {
 	sort.Sort(urllist(dois))
 	if dois[0].Title != titlefirst || dois[1].Title != titlesecond {
 		t.Fatalf("Failed secondary sorting by Title: %v", dois)
+	}
+}
+
+func TestMKSitemap(t *testing.T) {
+	targetpath, err := ioutil.TempDir("", "test_sitemap_cli")
+	if err != nil {
+		t.Fatalf("Failed to create sitemap cli temp directory: %v", err)
+	}
+	defer os.RemoveAll(targetpath)
+
+	cmd := setUpCommands("")
+
+	// check safe exit on non-existing output directory
+	cmd.SetArgs([]string{"make-sitemap", "-oidonotexist", "non-existing.xml"})
+	err = cmd.Execute()
+	if err != nil {
+		t.Fatalf("Error on non-existing sitemap directory: %s", err.Error())
+	}
+
+	// check safe exit on non-existing input file
+	cmd.SetArgs([]string{"make-sitemap", fmt.Sprintf("-o%s", targetpath), "non-existing.xml"})
+	err = cmd.Execute()
+	if err != nil {
+		t.Fatalf("Error on non-existing sitemap directory: %s", err.Error())
+	}
+	fi, err := ioutil.ReadDir(targetpath)
+	if err != nil {
+		t.Fatalf("Error on reading target dir: %s", err.Error())
+	}
+	if len(fi) != 0 {
+		t.Fatalf("Encountered unexpected number of files: %d/0", len(fi))
+	}
+
+	// create local test XML file server
+	server := serveDataciteXMLserver()
+	defer server.Close()
+
+	// check local test server works
+	serverURL, err := url.Parse(server.URL)
+	if err != nil {
+		t.Fatalf("Could not parse server URL: %q", serverURL)
+	}
+
+	// test error on invalid datacite content
+	testNonXML := fmt.Sprintf("%s/non-xml", server.URL)
+	cmd.SetArgs([]string{"make-sitemap", fmt.Sprintf("-o%s", targetpath), testNonXML})
+	err = cmd.Execute()
+	if err != nil {
+		t.Fatalf("Error on invalid sitemap file URL: %s", err.Error())
+	}
+	fi, err = ioutil.ReadDir(targetpath)
+	if err != nil {
+		t.Fatalf("Error on reading target dir: %s", err.Error())
+	}
+	if len(fi) != 0 {
+		t.Fatalf("Encountered unexpected number of files: %d/0", len(fi))
+	}
+
+	// test valid xml file handling
+	testXML := fmt.Sprintf("%s/xml", server.URL)
+	cmd.SetArgs([]string{"make-sitemap", fmt.Sprintf("-o%s", targetpath), testXML, testXML})
+	err = cmd.Execute()
+	if err != nil {
+		t.Fatalf("Error on invalid sitemap file URL: %s", err.Error())
+	}
+	targetFile := filepath.Join(targetpath, "urls.txt")
+	_, err = os.Stat(targetFile)
+	if errors.Is(err, os.ErrNotExist) {
+		t.Fatalf("Could not find sitemap file at: %s", targetFile)
+	} else if err != nil {
+		t.Fatalf("Unexpected error writing sitemap file: %s", err.Error())
 	}
 }
