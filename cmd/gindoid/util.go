1780d1a3d765eb0ebcdf583fd1aa3f8aab47899f
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-01 10:05:30 +0100

    [util] Add missingAnnexContent func
    The function checks whether an existing git annex repository
    in a local directory has annex files with locally missing
    file content.

--- cmd/gindoid/util.go
+++ cmd/gindoid/util.go
@@ -603,3 +603,21 @@ func remoteGitCMD(gitdir string, useannex bool, gitcmd ...string) (string, strin
 
 	return string(stdout), string(stderr), err
 }
+
+func missingAnnexContent(gitdir string) (bool, string, error) {
+	if _, err := os.Stat(gitdir); os.IsNotExist(err) {
+		return false, "", fmt.Errorf("path not found %q", gitdir)
+	}
+	// command should not return with an error or with any stderr content
+	// If stdout is empty, there is no missing content. If it is not empty,
+	// the number of lines correspond to the number of files with missing content.
+	stdout, stderr, err := remoteGitCMD(gitdir, true, "find", "--not", "--in=here")
+	if err != nil {
+		return false, "", err
+	} else if string(stderr) != "" {
+		return false, "", fmt.Errorf("git annex error: %s", string(stderr))
+	} else if string(stdout) == "" {
+		return false, "", nil
+	}
+	return true, string(stdout), nil
+}
