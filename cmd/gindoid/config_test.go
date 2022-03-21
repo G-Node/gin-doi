14e53484ebebb2beaf3503d9dccceb5de3046039
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-21 11:44:55 +0100

    [tests/config] Test loadconfig

--- cmd/gindoid/config_test.go
+++ cmd/gindoid/config_test.go
@@ -37,4 +37,31 @@ func TestLoadconfig(t *testing.T) {
 	} else if err != nil && !strings.Contains(err.Error(), "invalid web configuration") {
 		t.Fatalf("Error loading config: %q", err.Error())
 	}
+
+	// check invalid giturl
+	// we are not testing the gin-cli.config.ParseWebString here
+	err = os.Setenv("ginurl", "https://a.valid.url:1221")
+	if err != nil {
+		t.Fatalf("Error setting 'ginurl': %q", err.Error())
+	}
+	_, err = loadconfig()
+	if err == nil {
+		t.Fatal("Expected error on invalid 'giturl' env var")
+	} else if err != nil && !strings.Contains(err.Error(), "invalid git configuration") {
+		t.Fatalf("Error loading config: %q", err.Error())
+	}
+
+	// check error on invalid server setup
+	err = os.Setenv("giturl", "git@a.valid.url:2222")
+	if err != nil {
+		t.Fatalf("Error setting 'giturl': %q", err.Error())
+	}
+	_, err = loadconfig()
+	if err == nil {
+		t.Fatal("Expected error on invalid server configuration")
+	} else if err != nil && !strings.Contains(err.Error(), "no such host") {
+		t.Fatalf("Error loading config: %q", err.Error())
+	}
+
+	// further tests require a local git server.
 }
